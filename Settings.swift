ef85e219b50e7cd631e6f4a84c88dd8077a8b6ea
stardestro <topseceretagent@gmail.com>
2023-03-25 16:34:48 -0500

    Create Settings.swift

---
+++ Settings.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, settings!")
+        }
+    }
+}
+
