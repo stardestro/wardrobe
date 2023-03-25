bd932a9cb22327272816b87943f49bd81d3f5368
stardestro <topseceretagent@gmail.com>
2023-03-25 16:35:11 -0500

    Create Wardrobe.swift

---
+++ Wardrobe.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct Wardrobe: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
