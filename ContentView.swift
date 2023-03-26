4ee8d3823eb6dff752f3970f56a3d1b00a11f28a
Mateo Escamilla <91042644+2ie@users.noreply.github.com>
2023-03-26 01:29:07 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State var hatName: String
+    @State var hatName = "tshirt.fill"
+    //@State var topName: String
+    //@State var jacketName: String
+    //@State var bottomName: String
+    //@State var shoesName: String
+    //@State var temperatureValue: String 
+    
+    func get_algo_results() {
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        VStack(spacing: 10) {
+            ZStack {
+                // Top bar in ZStack
+                HStack {
+                    // Top horizontal bar
+                    Text("88*F")
+                        .padding()
+                        .background(
+                            Circle()
+                                .stroke(Color.primary, lineWidth: 3))
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                HStack {
+                    Text("WebThread")
+                        .font(.largeTitle)
+                        .padding()
+                }
+            }
+            // RoundedRectangle(cornerRadius: 14)
+            //    .stroke(Color.primary, lineWidth: 3)
+            //    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            // Image(systemName: "tshirt.fill")
+            //    .cornerRadius(7, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            //    .padding(30)
+            //    .background(Color.secondary)
+            //    .cornerRadius(10)
+            
+            // Need to remove systemName and only have self.hatName
+            Image(systemName: self.hatName)
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.primary, lineWidth: 5))
+            HStack {
+                // Shirt
+                Image(systemName: self.hatName)
+                    .resizable()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.primary, lineWidth: 5))
+                // Hat
+                Image(systemName: self.hatName)
+                    .resizable()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.primary, lineWidth: 5))
+            }
+            // Pants
+            Image(systemName: self.hatName)
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.primary, lineWidth: 5))
+            ZStack {
+                // Shoes and repeat button
+                HStack {
+                    // Shoes
+                    Image(systemName: self.hatName)
+                        .resizable()
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.primary, lineWidth: 5))
+                }
+                HStack {
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Button(action: { get_algo_results() }) {
+                            Image(systemName: "arrow.clockwise.circle")
+                                .font(.system(size: 60))
+                                .foregroundColor(.primary)
+                                .padding(.horizontal)
+                        }
+                    }
+                }
+            }
+        } //.padding(.bottom)
     }
 }
